086e7b394bb3031b51be254044db9b7fec8ac598
duydv9x <vuduy081293@gmail.com>
2017-09-17 15:43:52 +0700

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Dang Vu Duy on 9/17/17.
+//  Copyright © 2017 Dang Vu Duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
